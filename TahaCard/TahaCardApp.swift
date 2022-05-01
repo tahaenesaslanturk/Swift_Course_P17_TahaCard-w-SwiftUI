80647e9ce858c05f33bba6fa431587295fef5c2e
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-01 23:53:07 +0300

    Initial Commit

---
+++ TahaCard/TahaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahaCardApp.swift
+//  TahaCard
+//
+//  Created by Taha Enes Aslantürk on 1.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TahaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

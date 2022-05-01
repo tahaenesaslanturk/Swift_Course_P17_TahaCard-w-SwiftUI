aae7b339a52a278a529b057cc65b6b1ecea4b8af
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-02 00:33:37 +0300

    upd

--- TahaCard/ContentView.swift
+++ TahaCard/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("taha")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white,lineWidth: 5))
+                Text("Taha Enes Aslantürk")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                
+                InfoView(text: "+90 000 000 0000", imageName: "phone.fill")
+                InfoView(text: "tahaenesaslanturk@gmail.com", imageName: "envelope.fill")
+                
+            }
+        }
+        
     }
 }
 
---
+++ TahaCard/InfoView.swift
@@ -0,0 +1,35 @@
+//
+//  InfoView.swift
+//  TahaCard
+//
+//  Created by Taha Enes Aslantürk on 2.05.2022.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(Color(red: 0.09, green: 0.63, blue: 0.52))
+                Text(text)
+                    .bold()
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
